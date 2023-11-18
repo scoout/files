4b8c121a1f64c834127c049315710f27bff92c72
iqbal fasya <79214502+scoout@users.noreply.github.com>
2023-11-18 23:06:36 +0700

    Update 99-init-settings.sh

--- files/etc/uci-defaults/99-init-settings.sh
+++ files/etc/uci-defaults/99-init-settings.sh
@@ -1,1 +1,22 @@
+#!/bin/sh
 
+# lang
+uci set luci.main.lang=en
+uci commit luci
+
+# timezone
+uci set system.@system[0].timezone=WIB-7
+uci set system.@system[0].zonename=Asia/Jakarta
+
+# zram-swap
+uci set system.@system[0].zram_priority=100
+
+# ntp server
+uci -q delete system.ntp.server
+uci add_list system.ntp.server="time.indonesia.in"
+uci add_list system.ntp.server="ntp.indosat.com"
+uci add_list system.ntp.server="time3.jaring.id"
+uci add_list system.ntp.server="time.cloudflare.com"  # Cloudflare's NTP server
+uci commit system && service sysntpd reload
+
+exit 0
